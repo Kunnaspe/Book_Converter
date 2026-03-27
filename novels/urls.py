cb1c737fd4341d8aa48bb2b2116cf06cdb2e2c89
Kunnaspe <122162227+Kunnaspe@users.noreply.github.com>
2026-03-27 12:29:09 -0400

    Update urls.py

--- novels/urls.py
+++ novels/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from novels import views
 
-# Use the path:s3_key converter so keys with forward slashes such as moby_dick.txt are captured correctly
+# uses the path:s3_key converter so keys with forward slashes such as moby_dick.txt are captured correctly
 urlpatterns = [
     path('', views.novel_list, name='novel_list'),
     path('novel/<path:s3_key>/analyze/', views.novel_analyze, name='novel_analyze'),
